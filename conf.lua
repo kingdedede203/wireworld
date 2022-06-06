616163d03f8740fc0cec90e56123f6cbb4b63819
kingdedede203 <kingdedede203@gmail.com>
2022-06-06 18:09:23 +0400

    Added MSAA

--- conf.lua
+++ conf.lua
@@ -3,6 +3,7 @@ function love.conf(t)
 	t.version = "11.4"
 	t.window.width = 1280
 	t.window.height = 720
-	t.console = true  -- windows only
+	t.window.msaa = 4
+	t.console = true -- windows only
 	t.gammacorrection = true
 end
